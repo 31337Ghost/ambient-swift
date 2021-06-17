864fba1218e3fdb26c04d7dc4732f2c970c37c01
Pavel Golovin <golovinps@yandex.ru>
2021-06-17 22:17:57 +0300

    Initial Commit

---
+++ Shared/AmbientApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmbientApp.swift
+//  Shared
+//
+//  Created by Golovin Pavel on 17.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct AmbientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
